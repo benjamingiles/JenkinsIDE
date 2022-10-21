c59873a4ad41862c6e39d44850bb8b1bda549caf
Ben Giles <bengiles8090@gmail.com>
2022-10-21 14:30:37 -0600

    changed hook trigger

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -70,7 +70,6 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
         String randomString = UUID.randomUUID().toString();
         randomString = n.concat(randomString);
         return randomString;
