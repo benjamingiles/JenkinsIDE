d131039f630b60637f2264df305cc311b9206c35
Ben Giles <bengiles8090@gmail.com>
2022-10-21 17:14:11 -0600

    added main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void Main() {
+  public static void main(String[] args) {
 
     Calculator calculator = new Calculator();
     System.out.println("welcome to the calculator");
